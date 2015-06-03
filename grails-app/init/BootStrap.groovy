987c3e81726218342be1fd56ee63297a4940b7bb
AngieLaw <angie.law1987@gmail.com>
2015-06-02 23:24:23 -0700

    Add initial data in BootStrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,21 @@
+import grails.converters.JSON
+import grails.util.Environment
+import howsmydrive.Trip
+
 class BootStrap {
 
     def init = { servletContext ->
+        if(Environment.current == Environment.DEVELOPMENT) {
+
+            def json = '''[{"tripId":"1","endDate":"2015-05-27 00:00:00.678", "engineRpmMax":12,"speed":50,"startDate":"2015-05-27 00:00:00.678"   },
+                {"tripId":"2","endDate":"2015-05-27 00:00:00.678", "engineRpmMax":12,"speed":50,"startDate":"2015-05-27 00:00:00.678"   }]'''
+
+            def triplist = JSON.parse(json)
+            triplist.each {
+                def trip = new Trip(JSON.parse(it.toString()))
+                println trip.save(flush:true)
+            }
+        }
     }
     def destroy = {
     }
