c228b0938171bda1d3485324927d48f281d05ddc
Yong Cheng <yong.cheng.in@gmail.com>
2015-06-07 23:36:24 -0400

    Updated ObdReadingRestController with same logical in TripRestController.

--- grails-app/controllers/howsmydrive/ObdReadingRestController.groovy
+++ grails-app/controllers/howsmydrive/ObdReadingRestController.groovy
@@ -4,6 +4,7 @@ import com.goodriver.grails.MyErrors
 import grails.converters.JSON
 import grails.rest.RestfulController
 import grails.transaction.Transactional
+import org.springframework.validation.FieldError
 
 @Transactional
 class ObdReadingRestController extends RestfulController{
@@ -22,25 +23,62 @@ class ObdReadingRestController extends RestfulController{
 
     def postObdReading() {
         def o =  getObjectToBind()
+
+        def noError = true
+        def numberOfTrips = 0
+
+        println o.JSON
+        if (o.JSON.size() == 1 && o.JSON[0] && o.JSON[0].size() == 0) {
+            respond ([error: "no data"])
+        }
+
+        def tempTrip
+        def tripList = []
         def myErrors
-
         o.JSON.each {
-            println it.toString()
-            def obdReading = new ObdReading(JSON.parse(it.toString()))
+            def trip = new ObdReading(JSON.parse(it.toString()))
+            tripList.add(trip)
+            if (!trip.validate()) {
+                noError = false
+                tempTrip = trip
+                trip.errors.each {
+                    println it
+                }
 
-            if (obdReading.validate()) {
+                if (!myErrors)
+                    myErrors = new MyErrors()
 
-            } else {
-                respond obdReading
+
+                def checkedFields = new HashSet<String>()
+                trip.errors.fieldErrors.each {
+                    def fieldName = it.field
+                    if (!checkedFields.contains(fieldName)) {
+                        List<FieldError> fieldErrors = trip.errors.getFieldErrors(fieldName)
+
+                        if (fieldErrors.size() > 1) {
+                            fieldErrors.each {
+                                if(it.rejectedValue)
+                                    myErrors.addFieldError(it)
+                            }
+                        } else {
+                            myErrors.addFieldError(fieldErrors.get(0))
+                        }
+                    }
+                    checkedFields.add(fieldName)
+                }
+            };
+        }
+
+        if(noError) {
+            tripList.each {
+                it.save(flush:true)
             }
-
-
         }
 
-        if (myErrors) {
-            respond myErrors
-        } else {
-            respond([message: "Saved $o.JSON.obdReading.size obdReadings."])
+        if (noError) {
+            respond([message: "Saved $tripList.size obdReadings."])
         }
+
+        respond myErrors
     }
 }
